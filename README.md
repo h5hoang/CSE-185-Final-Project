53bee6e2a5881abe24912734517e49fa90b62d3e
h5hoang <h5hoang@ucsd.edu>
2023-05-17 14:33:32 -0700

    works!

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ sickle info stuff here!
 IF USING VSCODE, INSTALL BIOPYTHON WITH COMMAND:
 py -m pip install biopython
 py -m pip install fastq    
+
+
+https://biopython.org/docs/1.75/api/Bio.SeqIO.html#submodules 
+^ scroll down
--- sickle/utils.py
+++ sickle/utils.py
@@ -1,2 +1,10 @@
 import sys
 from Bio import SeqIO
+
+#testing to see if library is able to print out fq file!
+#for record in SeqIO.parse("example-files/NA12878_child_1.fq", "fastq"):
+#    print(record.id)
+
+for record in SeqIO.parse("example-files/NA12878_child_1.fq", "fastq"):
+    print(record.format("qual"))
+
