054f51dff7911a945c3f470e57c29fc4fbaa3f6e
h5hoang <h5hoang@ucsd.edu>
2023-05-17 14:06:15 -0700

    update

--- setup.py
+++ setup.py
@@ -1,3 +1,30 @@
 import os
+from setuptools import setup, find_packages
 
-# code to print info about tool we implemented
+# version-keeping code based on pybedtools
+curdir = os.path.abspath(os.path.dirname(__file__))
+MAJ = 0
+MIN = 0
+REV = 0
+VERSION = '%d.%d.%d' % (MAJ, MIN, REV)
+with open(os.path.join(curdir, 'sickle/version.py'), 'w') as fout:
+        fout.write(
+            "\n".join(["",
+                       "# THIS FILE IS GENERATED FROM SETUP.PY",
+                       "version = '{version}'",
+                       "__version__ = version"]).format(version=VERSION)
+        )
+        
+setup(
+    name='sickle',
+    version=VERSION,
+    description='CSE185 Final Project',
+    author='Kathy Gu, Pinyi Wang, Hanson Hoang',
+    author_email='kygu@ucsd.edu, p8wang@ucsd.edu, h5hoang@ucsd.edu',
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "sickle=sickle.sickle:main"
+        ],
+    },
+)
---
+++ sickle/utils.py
@@ -0,0 +1,2 @@
+import sys
+from Bio import SeqIO
