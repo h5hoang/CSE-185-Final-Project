2a3885940cb614525401e519a8302777d497f9d7
h5hoang <h5hoang@ucsd.edu>
2023-06-03 17:33:48 -0700

    setup

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ MAJ = 0
 MIN = 0
 REV = 0
 VERSION = '%d.%d.%d' % (MAJ, MIN, REV)
-with open(os.path.join(curdir, 'mysickle/version.py'), 'w') as fout:
+with open(os.path.join(curdir, 'sickle/version.py'), 'w') as fout:
         fout.write(
             "\n".join(["",
                        "# THIS FILE IS GENERATED FROM SETUP.PY",
@@ -24,7 +24,7 @@ setup(
     packages=find_packages(),
     entry_points={
         "console_scripts": [
-            "mysickle=mysickle.sickle:main"
+            "sickle=sickle.sickle:main"
         ],
     },
 )
